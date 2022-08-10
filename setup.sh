8168d788cedfa61145425cf2057af5131225b4d2
Adriana-Kornreuter <110978913+Adriana-Kornreuter@users.noreply.github.com>
2022-08-10 08:54:26 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prof.massaki@gmail.com\"\n\
+email = \"drika.kornreuter@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
